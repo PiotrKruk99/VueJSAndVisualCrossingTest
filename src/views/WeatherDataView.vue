38851128be7ef1f1b16d9f06429dc59042a30cac
PiotrKruk99-b <biuro@liberezo.pl>
2022-10-19 17:23:33 +0200

    working on response from api

--- src/views/WeatherDataView.vue
+++ src/views/WeatherDataView.vue
@@ -1,27 +1,39 @@
 <script lang="ts">
 import axios from "axios";
 
+interface ResultT {
+  resolvedAddress: string;
+}
+
+class Result {
+  resolvedAddress!: string;
+}
+
 export default {
   data() {
     return {
       uri: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/leszno/2022-10-18/2022-10-19?unitGroup=metric&include=days%2Ccurrent&key=4NCLQ7CWLBLG539SD2U4YZUB9&contentType=json",
-      result: "",
-      resultObj: JSON,
+      //result: any,
+      resultObj: null,
     };
   },
   methods: {
-    onClick() {
-      this.result = "qwertys";
-      axios.get(this.uri).then((responce) => {
+    async onClick() {
+      //this.result = "qwertys";
+      await axios.get(this.uri).then((responce) => {
+        console.log(responce);
         this.result = responce.data;
-        this.resultObj = JSON.parse(this.result);
+        //this.resultObj = JSON.parse(this.result);
+        //var lalala = JSON.parse(this.result);
       });
     },
   },
   setup() {
     const bleee = "tralalala";
+    var result = null;
     return {
       bleee,
+      result,
     };
   },
 };
@@ -30,11 +42,16 @@ export default {
 <template>
   <div class="weatherData">
     <h2>Data from Visual Weather Forecast {{ bleee }}</h2>
-    <p>i coś jeszcze testowego do obrobienia {{ result }}</p>
     <p>
-      Miasto: {{ resultObj.resolvedAddress ? resultObj.resolvedAddress : "error" }}
+      Miasto:
+      {{ resultObj ? resultObj.resolvedAddress : "error" }}
+    </p>
+    <p>
+      Miasto:
+      {{ result ? result.resolvedAddress : "" }}
     </p>
     <button v-on:click="onClick">Kliknij mnie</button>
+    <p>i coś jeszcze testowego do obrobienia {{ result }}</p>
   </div>
 </template>
 
