a3056610a3bd90f763d7f9d847ec5272597b06df
PiotrKruk99-d <biuro@liberezo.pl>
2022-10-20 02:09:22 +0200

    getting main data and current day data through class works

--- src/views/WeatherDataView.vue
+++ src/views/WeatherDataView.vue
@@ -1,39 +1,65 @@
 <script lang="ts">
 import axios from "axios";
 
-interface ResultT {
-  resolvedAddress: string;
-}
-
-class Result {
-  resolvedAddress!: string;
+class CurrentConditions {
+  datetime?: string;
+  datetimeEpoch?: number;
+  temp?: number;
+  feelslike?: number;
+  humidity?: number;
+  dew?: number;
+  precip?: number;
+  precipprob?: any;
+  snow?: number;
+  snowdepth?: number;
+  preciptype?: any;
+  windgust?: any;
+  windspeed?: number;
+  winddir?: number;
+  pressure?: number;
+  visibility?: number;
+  cloudcover?: number;
+  solarradiation?: number;
+  solarenergy?: any;
+  uvindex?: number;
+  conditions?: string;
+  icon?: string;
+  stations?: string[];
+  sunrise?: string;
+  sunriseEpoch?: number;
+  sunset?: string;
+  sunsetEpoch?: number;
+  moonphase?: number;
 }
 
 export default {
   data() {
     return {
-      uri: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/leszno/2022-10-18/2022-10-19?unitGroup=metric&include=days%2Ccurrent&key=4NCLQ7CWLBLG539SD2U4YZUB9&contentType=json",
-      //result: any,
-      resultObj: null,
+      //uri: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/leszno/2022-10-18/2022-10-19?unitGroup=metric&include=days%2Ccurrent&key=4NCLQ7CWLBLG539SD2U4YZUB9&contentType=json",
+      uri: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/leszno?unitGroup=metric&include=current&key=4NCLQ7CWLBLG539SD2U4YZUB9&contentType=json",
+      result: null,
+      current: new CurrentConditions(),
     };
   },
   methods: {
     async onClick() {
-      //this.result = "qwertys";
-      await axios.get(this.uri).then((responce) => {
-        console.log(responce);
-        this.result = responce.data;
-        //this.resultObj = JSON.parse(this.result);
-        //var lalala = JSON.parse(this.result);
-      });
+      await axios
+        .get(this.uri)
+        .then((response) => {
+          console.log(response);
+          this.result = response.data;
+          this.current = response.data.currentConditions;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.result = null;
+        });
     },
   },
   setup() {
-    const bleee = "tralalala";
-    var result = null;
+    //var current: CurrentConditions;
     return {
-      bleee,
-      result,
+      //current,
     };
   },
 };
@@ -41,17 +67,17 @@ export default {
 
 <template>
   <div class="weatherData">
-    <h2>Data from Visual Weather Forecast {{ bleee }}</h2>
     <p>
       Miasto:
-      {{ resultObj ? resultObj.resolvedAddress : "error" }}
+      {{ result?.[`resolvedAddress`] }}, szerokość geograficzna:
+      {{ result?.[`latitude`] }}, długość geograficzna:
+      {{ result?.[`longitude`] }}
     </p>
     <p>
-      Miasto:
-      {{ result ? result.resolvedAddress : "" }}
+      Aktualna temperatura to {{ current?.temp }}, and wind speed is
+      {{ current?.windspeed }}
     </p>
-    <button v-on:click="onClick">Kliknij mnie</button>
-    <p>i coś jeszcze testowego do obrobienia {{ result }}</p>
+    <button v-on:click="onClick">Get weather data</button>
   </div>
 </template>
 
