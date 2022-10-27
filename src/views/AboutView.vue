5a84df675ef804ffdb0145a59668e59c19ba00f9
PiotrKruk99-b <biuro@liberezo.pl>
2022-10-27 14:59:44 +0200

    moving stored data from one view to another tested and works

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      This is an about page and test of getting store value from another page.
+      Entered city name is {{ weatherValues.city }}
+    </h1>
   </div>
 </template>
+
+<script lang="ts">
+import { weatherStore } from "@/stores/weather";
+
+export default {
+  data() {
+    return {
+      weatherValues: weatherStore(),
+    };
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
